4de7de55f4650e42ef719deefd99a16c4ee870bf
The Banished Prince <thebanishedprince15@protonmail.com>
2025-07-16 01:44:22 +0530

    Refactored: Flattened project structure for Streamlit Cloud

--- streamlit_ui/brief_ui.py
+++ streamlit_ui/brief_ui.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from agent2.brief_parser import parse_brief
-from agent2.gdrive_uploader import create_drive_folders
+from brief_parser import parse_brief
+from gdrive_uploader import create_drive_folders
 
 st.title("Creative Brief → Google Drive Folder Creator")
 
